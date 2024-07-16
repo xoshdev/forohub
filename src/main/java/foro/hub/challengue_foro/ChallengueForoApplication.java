40ddbd28264e02e8a6809f6c81fd84735ae2c688
Xosh <xoshdev@gmail.com>
2024-07-16 19:19:02 -0400

    Proyecto Challengue Foro Hub

---
+++ src/main/java/foro/hub/challengue_foro/ChallengueForoApplication.java
@@ -0,0 +1,13 @@
+package foro.hub.challengue_foro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengueForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengueForoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/foro/hub/challengue_foro/ChallengueForoApplicationTests.java
@@ -0,0 +1,13 @@
+package foro.hub.challengue_foro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengueForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
